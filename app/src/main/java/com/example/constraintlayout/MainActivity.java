2406197842711e571c9bb1206bb5c0c31b511c6e
AdanaSyaifullah <muhammadadanas000@gmail.com>
2021-03-09 18:50:40 +0700

    Activity 2

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -40,6 +40,16 @@ public class MainActivity extends AppCompatActivity {
                             "LOGIN SUKSES || email : " + nama + " password : " + password + "", Toast.LENGTH_LONG);
                     t.show();
                 }
+                else if (!edemail.getText().toString().equals("admin@mail.com")) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                           " Email salah ", Toast.LENGTH_SHORT);
+                    t.show();
+                }
+                else if (!edpassword.getText().toString().equals("123")) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            " Password salah ", Toast.LENGTH_SHORT);
+                    t.show();
+                }
                 else{
                     Toast.makeText((getApplicationContext()), "Username atau Password Anda salah",
                             Toast.LENGTH_SHORT).show();
